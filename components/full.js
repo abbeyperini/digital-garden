21b22b000576c99de64044e50eb99410b8f61efe
abbeyperini <abbeyperini@gmail.com>
2023-07-12 18:52:23 -0400

    sorting, refactor, homepage styling

---
+++ components/full.js
@@ -0,0 +1,153 @@
+class Full extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  async connectedCallback() {
+    this.setAttribute("class", "full-component");
+    function formatMetadata(blogName) {
+      const topicsLinks = allBlogs[blogName].topics.map((topic, index) => {
+        let linkText = allTopics[topic].title
+        if (index !== (allBlogs[blogName].topics.length - 1)) {
+          linkText += ","
+        }
+        return `<li><a href="/topic.html?topic=${topic}">${linkText}</a></li>`
+      });
+      const topicsList = `<div class="topics-container"><p>Topics:</p><ul class="topics-list">${topicsLinks.join("")}</ul></div>`
+      let seriesList = null;
+      if (allBlogs[blogName].series.length > 0) {
+        const allSeries = data.series;
+        const seriesLinks = allBlogs[blogName].series.map((series, index) => {
+          let linkText = allSeries[series].title
+          if (index !== (allBlogs[blogName].series.length - 1)) {
+            linkText += ",";
+          }
+          return `<li><a href="/series.html?series=${series}">${linkText}</a></li>`;
+        })
+        seriesList = `<div class="series-container"><p>Series:</p><ul class="series-list">${seriesLinks.join("")}</ul></div>`;
+      }
+      return seriesList ? `<div class="metadata-container"><p>Knowledge Level: ${allBlogs[blogName]["knowledge-level"]}</p><p>Planted: ${new Date(allBlogs[blogName].planted).toDateString()}<p>Last Tended: ${new Date(allBlogs[blogName]["last-tended"]).toDateString()}${topicsList}${seriesList}</div>` : `<div class="metadata-container"><p>Knowledge Level: ${allBlogs[blogName]["knowledge-level"]}</p><p>Planted: ${new Date(allBlogs[blogName].planted).toDateString()}<p>Last Tended: ${new Date(allBlogs[blogName]["last-tended"]).toDateString()}${topicsList}</div>`
+    }
+
+    function formatZero(blogName) {
+      const page = `../blogs/${blogName}.md`;
+      return `<zero-md id="zero" src=${page}></zero-md>`;
+    }
+
+    function getName(type) {
+      let name = params.get(type);
+      if (!name) {
+        name = this.attributes[type].value;
+      }
+      return name;
+    }
+
+    function formatSingleBlog() {
+      const blogName = getName("blog")
+      const blog = formatZero(blogName);
+      const metaData = formatMetadata(blogName);
+      const pageTitle = `${allBlogs[blogName].title} by Abbey Perini`;
+      const content = `<h1 class="title">${pageTitle} by Abbey Perini</h1><div class="blog-container">${metaData}${blog}</div>`;
+      return { content, pageTitle };
+    }
+
+    function formatSeries() {
+      const series = getName("series");
+      const seriesHTML = allSeries[series].articles.map((blogName) => {
+        const blog = formatZero(blogName);
+        const metaData = formatMetadata(blogName);
+        return `<div class="blog-container">${metaData}${blog}</div>`;
+      })
+      const finalHTML = seriesHTML.join("");
+      const pageTitle = `${allSeries[series].title} Series by Abbey Perini`;
+      const content = `<h1 class="title">${pageTitle}</h1>${finalHTML}`;
+      return { content, pageTitle };
+    }
+
+    function formatTopic() {
+      const topic = getName("topic")
+      const topicHTML = allTopics[topic].articles.map((blogName) => {
+        const blog = formatZero(blogName);
+        const metaData = formatMetadata(blogName);
+        return `<div class="blog-container">${metaData}${blog}</div>`;
+      })
+      const finalHTML = topicHTML.join("");
+      const pageTitle = `Abbey Perini's Articles About ${allTopics[topic].title}`
+      const content = `<h1 class="title">${pageTitle}</h1>${finalHTML}`;
+      return { content, pageTitle };
+    }
+
+    function comparePlantedDates(a, b, type) {
+      const aDate = new Date(allBlogs[a].planted).getTime();
+      const bDate = new Date(allBlogs[b].planted).getTime();
+      if (bDate > aDate) {
+        return 1;
+      }
+      if (bDate < aDate) {
+        return -1;
+      }
+      return 0;
+    }
+
+    function formatSort() {
+      const sortType = getName("sort");
+      let blogNames;
+      const keys = Object.keys(allBlogs);
+      // sorted by most recent
+      if (sortType === "planted") {
+        blogNames = keys.sort(comparePlantedDates)
+      }
+      const sortedHTML = blogNames.map((blogName) => {
+        const blog = formatZero(blogName);
+        const metaData = formatMetadata(blogName);
+        return `<div class="blog-container">${metaData}${blog}</div>`;
+      })
+      const finalHTML = sortedHTML.join("");
+      const pageTitle = `All of Abbey Perini's Articles Sorted by Most Recently Planted`;
+      const content = `<h1 class="title">${pageTitle}</h1>${finalHTML}`;
+      return { content, pageTitle };
+    }
+
+    const data = await fetch('https://images.abbeyperini.com/data/blog-data.json').then((response) => {return response.json()});
+    const allBlogs = data.blogs;
+    const allSeries = data.series;
+    const allTopics = data.topics;
+    const attributes = this.attributes;
+    const params = new URLSearchParams(document.location.search);
+
+    function setPageTitles(pageTitle) {
+      document.title = pageTitle;
+      document.querySelector('meta[property="og:title"]').setAttribute("content", pageTitle);
+    }
+
+      if (!!(attributes.blog || params.get("blog"))) {
+        const { content, pageTitle } = formatSingleBlog();
+        this.innerHTML = content;
+        setPageTitles(pageTitle);
+        return;
+      }
+
+      if (!!(attributes.series || params.get("series"))) {
+        const { content, pageTitle } = formatSeries();
+        this.innerHTML = content;
+        setPageTitles(pageTitle);
+        return;
+      }
+
+      if (!!(attributes.topic || params.get("topic"))) {
+        const { content, pageTitle } = formatTopic();
+        this.innerHTML = content;
+        setPageTitles(pageTitle);
+        return;
+      }
+
+      if (!!(attributes.sort || params.get("sort"))) {
+        const { content, pageTitle } = formatSort();
+        this.innerHTML = content;
+        setPageTitles(pageTitle);
+        return;
+      }
+    }
+}
+
+customElements.define('full-component', Full);
--- components/preview.js
+++ components/preview.js
@@ -10,25 +10,67 @@ class Preview extends HTMLElement {
     const allSeries = data.series;
     const attributes = this.attributes;
 
+    function mapBlogsToLinks(array) {
+      return array.map((blogName, index) => {
+        if (index < 4) {
+          return `<li><h3><a href="/blog.html?blog=${blogName}">${allBlogs[blogName].title}</a></h3><li>`;
+        }
+      })
+    }
+
+    function comparePlantedDates(a, b, type) {
+      const aDate = new Date(allBlogs[a].planted).getTime();
+      const bDate = new Date(allBlogs[b].planted).getTime();
+      if (bDate > aDate) {
+        return 1;
+      }
+      if (bDate < aDate) {
+        return -1;
+      }
+      return 0;
+    }
+
+    if (attributes.sort) {
+      const sortType = attributes.sort.value;
+      let blogNames;
+      let links;
+      // sorted by most recent
+      if (sortType === "planted") {
+        const keys = Object.keys(allBlogs);
+        blogNames = keys.sort(comparePlantedDates)
+        links = mapBlogsToLinks(blogNames);
+      }
+      links.push(`<a href="/sort.html?sort=${sortType}">Read all</a>`);
+      return this.innerHTML = `<ul>${links.join("")}</ul>`;
+    }
+
     if (attributes.blog) {
       const blogName = attributes.blog.value;
-      this.innerHTML = `<h3><a href="/blog.html?blog=${blogName}">${allBlogs[blogName].title}</a></h3>`
+      return this.innerHTML = `<h3><a href="/blog.html?blog=${blogName}">${allBlogs[blogName].title}</a></h3>`;
     }
 
     if (attributes.series) {
       const series = attributes.series.value;
-      const seriesTitles = allSeries[series].articles.map((blogName) => {
-        return `<h3><a href="/blog.html?blog=${blogName}">${allBlogs[blogName].title}</a></h3>`
-      })
-      this.innerHTML = seriesTitles.join("");
+      const seriesLinks = mapBlogsToLinks(allSeries[series].articles);
+      if (allSeries[series].articles.length > 3) {
+        seriesLinks.push(`<a href="/series.html?series=${series}">Read more</a>`);
+      }
+      if (allSeries[series].articles.length <= 3) {
+        seriesLinks.push(`<a href="/series.html?series=${series}">Read all</a>`);
+      }
+      return this.innerHTML = `<ul>${seriesLinks.join("")}</ul>`;
     }
 
     if (attributes.topic) {
       const topic = attributes.topic.value;
-      const topicTitles = allTopics[topic].articles.map((blogName) => {
-        return `<h3><a href="/blog.html?blog=${blogName}">${allBlogs[blogName].title}</a></h3>`
-      })
-      this.innerHTML = topicTitles.join("");
+      const topicLinks = mapBlogsToLinks(allTopics[topic].articles);
+      if (allTopics[topic].articles.length > 3) {
+        topicLinks.push(`<a href="/topic.html?topic=${topic}">Read more</a>`);
+      }
+      if (allTopics[topic].articles.length <= 3) {
+        topicLinks.push(`<a href="/topic.html?topic=${topic}">Read all</a>`);
+      }
+      return this.innerHTML = `<ul>${topicLinks.join("")}</ul>`;
     }
 
     this.setAttribute("class", "preview-component");
