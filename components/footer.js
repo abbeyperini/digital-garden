74fe21443c001472d22636f9a2ffa2c38f860102
abbeyperini <abbeyperini@gmail.com>
2023-07-14 13:32:13 -0400

    fix: CSS

--- components/footer.js
+++ components/footer.js
@@ -13,22 +13,22 @@ class Footer extends HTMLElement {
             <a href="https://abbeyperini.dev">Portfolio</a>
           </li>
           <li>
-            <a href="https://dev.to/abbeyperini">DEV</a><img src="https://images.abbeyperini.com/logos/devto.svg" alt="" width="30">
+            <img src="https://images.abbeyperini.com/logos/devto.svg" alt="" width="30"><a href="https://dev.to/abbeyperini">DEV</a>
           </li>
           <li>
-            <a href="https://abbeyperini.hashnode.dev/">Hashnode</a><img src="https://images.abbeyperini.com/logos/hashnode-icon.png" alt="" width="30">
+            <img src="https://images.abbeyperini.com/logos/hashnode-icon.png" alt="" width="30"><a href="https://abbeyperini.hashnode.dev/">Hashnode</a>
           </li>
           <li>
-            <a href="https://github.com/abbeyperini">Github</a><img src="https://images.abbeyperini.com/logos/github.png" alt="" width="30">
+            <img src="https://images.abbeyperini.com/logos/github.png" alt="" width="30"><a href="https://github.com/abbeyperini">Github</a>
           </li>
           <li class="footer-linkedin">
-            <a href="https://www.linkedin.com/in/abbey-perini/">LinkedIn</a><img src="https://images.abbeyperini.com/logos/Linked-logo.png" alt="" width="30">
+            <img src="https://images.abbeyperini.com/logos/Linked-logo.png" alt="" width="30"><a href="https://www.linkedin.com/in/abbey-perini/">LinkedIn</a>
           </li>
           <li class="twitter">
-            <a href="https://twitter.com/AbbeyPerini">Twitter</a><img src="https://images.abbeyperini.com/logos/Twitter.png" alt="" width="30">
+            <img src="https://images.abbeyperini.com/logos/Twitter.png" alt="" width="30"><a href="https://twitter.com/AbbeyPerini">Twitter</a>
           </li>
+          <li><p class="copyright"><span class"copyright-symbol">&copy;</span>${year}</p></li>
         </ul>
-        <p class="copyright">&copy; ${year}</p>
       </footer>
     `;
     this.setAttribute("class", "footer-component");
