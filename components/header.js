1e7813a4c971cb66defb577d0dc252ad47508f90
abbeyperini <abbeyperini@gmail.com>
2023-11-15 11:53:55 -0500

    feat: first pass, picking photos

--- components/header.js
+++ components/header.js
@@ -17,6 +17,8 @@ class Header extends HTMLElement {
             <li><a href="https://github.com/abbeyperini/digital-garden">The Digital Garden on Github</a></li>
             <li><a href="https://abbeyperini.dev">About Abbey</a></li>
             <li><a href="https://abbey-perinis-stickers.printify.me/products">Stickers</a></li>
+            <li><a href="/speaking.html">Abbey's Speaking</a></li>
+            <li><a href="/conference-wall.html>Conference Wall</a></li>
           </ul>
         </nav>
         <input onchange="switchTheme(event)" type="checkbox" id="theme-toggle" aria-label="Toggle Color Theme" class="theme-switch" />
