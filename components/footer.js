548ff5db37df31c2785a9624b481d83ca156f0cb
abbeyperini <abbeyperini@gmail.com>
2024-01-12 13:53:28 -0500

    update contact in footer

--- components/footer.js
+++ components/footer.js
@@ -10,22 +10,7 @@ class Footer extends HTMLElement {
       <footer>
         <ul class="list footer-list">
           <li>
-            <a href="https://abbeyperini.dev">Portfolio</a>
-          </li>
-          <li>
-            <img src="https://images.abbeyperini.com/logos/devto.svg" alt="" width="30"><a href="https://dev.to/abbeyperini">DEV</a>
-          </li>
-          <li>
-            <img src="https://images.abbeyperini.com/logos/hashnode-icon.png" alt="" width="30"><a href="https://abbeyperini.hashnode.dev/">Hashnode</a>
-          </li>
-          <li>
-            <img src="https://images.abbeyperini.com/logos/github.png" alt="" width="30"><a href="https://github.com/abbeyperini">Github</a>
-          </li>
-          <li class="footer-linkedin">
-            <img src="https://images.abbeyperini.com/logos/Linked-logo.png" alt="" width="30"><a href="https://www.linkedin.com/in/abbey-perini/">LinkedIn</a>
-          </li>
-          <li class="twitter">
-            <img src="https://images.abbeyperini.com/logos/Twitter.png" alt="" width="30"><a href="https://twitter.com/AbbeyPerini">Twitter</a>
+            <a href="https://abbeyperini.dev/contact">Contact</a>
           </li>
           <li><p class="copyright"><span class"copyright-symbol">&copy;</span>${year}</p></li>
         </ul>
